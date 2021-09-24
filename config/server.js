4dec8910dd297ff0c1ec3451fdfb065572c86e82
xlaminent <xlaminent@gmail.com>
2021-09-24 15:07:39 +0200

    added messages

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '02c8ae6aa94de991b9df3d7be1b2dd90'),
